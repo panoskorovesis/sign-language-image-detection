a183b3a875085e7f127d45c8860c6e1764c4997f
Panos Korovesis <panoskorovesis@outlook.com>
2024-12-22 12:03:47 +0000

    Replace conda with native python

--- setup.sh
+++ setup.sh
@@ -2,35 +2,24 @@
 
 set -e
 
-# Check if conda is installed
-# ! command checks if the command will run or fail
-if ! command -v conda &> /dev/null; then
-    echo "Conda is not installed. Please install Anaconda or Miniconda."
-    exit 1
+if [ -d ./.venv ]; then
+    echo "Deleting existing VirtualEnv"
+    rm -rf ./.venv
 fi
 
-# source the env to be able to do activate / deactivate
-source /opt/conda/etc/profile.d/conda.sh
-
-if conda-env list | grep "SL-Detection"; then
-    echo "Deleting existing 'SL-Detection' conda environment"
-    conda deactivate
-    conda remove --name SL-Detection --all --yes
-fi
-
-# Create a new conda environment named
-echo "Creating new conda environment 'SL-Detection'..."
-conda create --name SL-Detection -y
-echo "Conda environment 'SL-Detection' has been created successfully."
+# Create a new python virtual env
+echo "Creating new python virtual environment"
+python3 -m venv ./.venv
+echo "Python virtual environment created"
 
 # Activate the environment
-echo "Activating SL-Detection..."
-conda activate SL-Detection
-echo "Conda environment activated"
+echo "Activating VirtualEnv..."
+source ./.venv/bin/activate
+echo "VirtualEnv activated"
 
 # Install the requirements
 echo "Installing requirements"
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 echo "Requirements installed"
 
 # Finally download the dataset from kaggle
@@ -41,10 +30,10 @@ echo "Download Completed. Will unzip"
 
 # Unzip it
 echo "Will unzip dataset to sign_datasets/sign-language-gesture-images-dataset"
-unzip ./sign_datasets/sign-language-gesture-images-dataset.zip -d "sign_datasets/sign-language-gesture-images-dataset"
+unzip -oq ./sign_datasets/sign-language-gesture-images-dataset.zip -d "sign_datasets/sign-language-gesture-images-dataset"
 echo "Extraction completed"
 
 # Finally remove the zip
 echo "Will remove zip file"
 rm ./sign_datasets/sign-language-gesture-images-dataset.zip
-echo "Setup completed!"
+echo "Setup completed"
